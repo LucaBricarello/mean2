1ea5dcd4998f9ee84e19c43d1d448803e1dcdd4c
Luca <luca.bricarello@outlook.it>
2023-03-23 10:46:26 +0100

    added complex mean function

--- mean.c
+++ mean.c
@@ -1,4 +1,17 @@
+#include "mean.h"
+
 float mean(int a, int b)
 {
 	return (a + b) / 2;
+}
+
+
+complexi_float_t ComplexMean(complexi_int_t a, complexi_int_t b)
+{
+	complexi_float_t res;
+
+	res.re = mean(a.re, b.re);
+	res.im = mean(a.im, b.im);
+
+	return res;
 }
--- mean.h
+++ mean.h
@@ -1,6 +1,20 @@
 #ifndef _MEANH_
 #define _MEANH_
 
+typedef struct complexi_int
+{
+	int re;
+	int im;
+}complexi_int_t;
+
+typedef struct complexi_float
+{
+	float re;
+	float im;
+}complexi_float_t;
+
 float mean(int a, int b);
+
+complexi_float_t ComplexMean(complexi_int_t a, complexi_int_t b);
 
 #endif
